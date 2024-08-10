daf5740bd9d86a5cf8b2c595a22c07b2718cf0f2
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-08-10 13:31:53 +0100

    test(main.cpp): added some prints to check add and contain methods

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "YourSet.h"
 
 int main() {
+
+    // Create a new set
+    YourSet set;
+
+    // Add some values to the set
+    set.add("banana");
+    set.add("apple");
+    set.add("orange");
+
+    // Check if the set contains some values
+    std::cout << "Does set contain 'banana' ? " << (set.contains("banana") ? "Yes" : "No") << std::endl;
+    std::cout << "Does set contain 'apple' ? " << (set.contains("apple") ? "Yes" : "No") << std::endl;
+    std::cout << "Does set contain 'orange' ? " << (set.contains("orange") ? "Yes" : "No") << std::endl;
+    std::cout << "Does set contain 'watermelon' ? " << (set.contains("watermelon") ? "Yes" : "No") << std::endl;
+
+
     return 0;
 }
