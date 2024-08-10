fab8120a637e313f376ad7c9817a6c3c0cfa18a7
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-08-10 13:46:22 +0100

    test(main.cpp): test the add and remove methods

--- main.cpp
+++ main.cpp
@@ -12,10 +12,22 @@ int main() {
     set.add("orange");
 
     // Check if the set contains some values
+    std::cout << "Adding 'banana', 'apple' and 'orange' to the set" << std::endl;
     std::cout << "Does set contain 'banana' ? " << (set.contains("banana") ? "Yes" : "No") << std::endl;
     std::cout << "Does set contain 'apple' ? " << (set.contains("apple") ? "Yes" : "No") << std::endl;
     std::cout << "Does set contain 'orange' ? " << (set.contains("orange") ? "Yes" : "No") << std::endl;
     std::cout << "Does set contain 'watermelon' ? " << (set.contains("watermelon") ? "Yes" : "No") << std::endl;
+
+    // Remove some values from the set
+    std::cout << "Removing 'banana' from the set" << std::endl;
+    set.remove("banana");
+    std::cout << "Does set contain 'banana' ? " << (set.contains("banana") ? "Yes" : "No") << std::endl;
+
+    // Try to remove a value that is not in the set
+    std::cout << "Removing 'banana' from the set : " << (set.remove("banana") ? "Yes" : "No") << std::endl;
+
+    // Try to add a value that is already in the set
+    std::cout << "Adding 'apple' to the set : " << (set.add("apple") ? "Yes" : "No") << std::endl;
 
 
     return 0;
