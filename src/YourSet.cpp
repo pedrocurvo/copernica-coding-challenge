f25ab08618725688078a8983f54d2a8ac52f7c7d
Pedro Curvo <pedro.mpcurvo@gmail.com>
2024-08-10 13:14:04 +0100

    feat(YourSet): added private contains method to search in the BST

--- src/YourSet.cpp
+++ src/YourSet.cpp
@@ -1,1 +1,22 @@
 #include "YourSet.h"
+
+
+/// @brief Private function to evaluate if the BST contains the value. 
+/// @param node The node to start the search.
+/// @param value The value to search for.
+/// @return A boolean indicating if the value is in the BST.
+bool YourSet::contains(const Node* node, const std::string& value) const {
+    if (node == nullptr) {
+        return false;
+    }
+
+    if (node->data == value) {
+        return true;
+    }
+
+    if (value < node->data) { // < in strings? Yes, it compares the strings alphabetically
+        return contains(node->left.get(), value);
+    } else {
+        return contains(node->right.get(), value);
+    }
+}
